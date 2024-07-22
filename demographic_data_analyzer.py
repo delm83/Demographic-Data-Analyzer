ccacc39af654d28c65b7449cf31c0cbe3e3369c7
delm83 <delm83@googlemail.com>
2024-07-22 18:52:50 +0100

    calculated country with highest percentage of people who earn over 50K

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -36,8 +36,8 @@ def calculate_demographic_data(print_data=True):
     rich_percentage = (num_min_workers[num_min_workers['salary'] == '>50K']['salary'].count()/num_min_workers['salary'].count()*100).round(1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    highest_earning_country = df[df['salary'] == '>50K']['native-country'].value_counts().index[0]
+    highest_earning_country_percentage = (df[(df['salary']=='>50K') & (df['native-country']== highest_earning_country)]['salary'].count()/df[df['salary']=='>50K']['salary'].count()*100).round(1)
 
     # Identify the most popular occupation for those who earn >50K in India.
     top_IN_occupation = None
